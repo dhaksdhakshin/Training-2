487710ee6f690e64e6b18bef15ca9f3cd7232f4b
Dhakshin V <163077190+dhaksdhakshin@users.noreply.github.com>
2025-04-28 08:53:48 +0530

    Update continuous_deployment_pipeline.py.py

--- Level_20/Task-4/pipelines/continuous_deployment_pipeline.py.py
+++ Level_20/Task-4/pipelines/continuous_deployment_pipeline.py.py
@@ -9,17 +9,13 @@ def continuous_deployment_pipeline(
     model_deployer
 ):
     """Defines the continuous deployment pipeline."""
-    # Step 1: Ingest data
+
     X_train, X_test, y_train, y_test = data_ingestion()
     
-    # Step 2: Train the model
     model = model_training(X_train, y_train)
     
-    # Step 3: Evaluate the model
     r2_score = model_evaluation(model, X_test, y_test)
     
-    # Step 4: Trigger deployment decision
     deploy = deployment_trigger(r2_score)
     
-    # Step 5: Deploy the model (conditional)
     model_deployer(deploy=deploy, model=model)
